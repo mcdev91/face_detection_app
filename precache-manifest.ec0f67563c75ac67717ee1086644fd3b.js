55d5f84c2b72faad77c09153914c462f23a5dff0
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-03-25 23:04:47 +0200

    Updates

---
+++ precache-manifest.ec0f67563c75ac67717ee1086644fd3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1a787277aa4b56807fa043ed1e2437",
+    "url": "/face_detection_app/index.html"
+  },
+  {
+    "revision": "b5e22ef480dd5944e12e",
+    "url": "/face_detection_app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e934f49483c546804965",
+    "url": "/face_detection_app/static/css/main.3d8657b1.chunk.css"
+  },
+  {
+    "revision": "b5e22ef480dd5944e12e",
+    "url": "/face_detection_app/static/js/2.e290423c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face_detection_app/static/js/2.e290423c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e934f49483c546804965",
+    "url": "/face_detection_app/static/js/main.a80839f1.chunk.js"
+  },
+  {
+    "revision": "23270fc4141f210b1f1d",
+    "url": "/face_detection_app/static/js/runtime-main.ee1b33b6.js"
+  },
+  {
+    "revision": "4fce0fc30a21436b4a4c09e92b9c8a35",
+    "url": "/face_detection_app/static/media/LogoPng.4fce0fc3.png"
+  }
+]);
