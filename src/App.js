f0df6b983d063d33c761b963fe52acfdc72d5c0c
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-03-25 22:38:28 +0200

    UI

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const ParticlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 700
+      }
+    }
+  }
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Particles className="particles"
+        params={ParticlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+
+      {/*      
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className=''>
+            <div className='white f3 '>
+                {'User, you rank is...'}
+            </div>
+            <div className='white f1 '>
+                {'#1'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
